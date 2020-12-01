f73492936157f878e579f51b9686949959a98df4
Wilmer <wilmer.payall@gmail.com>
2020-12-01 13:28:10 -0400

    handle scroll move

--- components/Menu.js
+++ components/Menu.js
@@ -7,15 +7,32 @@ import styles from 'styles/Menu.module.sass';
 const Menu = ({ categories }) => {
   const sectionIds = categories.map((category) => category.nombreCategoria);
 
+  const handleOnUpdate = (el) => {
+    const match = window.matchMedia('(max-width: 1024px)');
+
+    if (el && match.matches) {
+      const item = document.querySelector(`#menu ul #li-${el.id}`);
+
+      if (item) {
+        item.scrollIntoView({ block: 'nearest' });
+      }
+    }
+  };
+
   return (
-    <div className={styles.container} stickyoffset={{ top: 10 }}>
+    <div className={styles.container} stickyoffset={{ top: 10 }} id="menu">
       <h2 className="h5 mb-3 d-none d-xl-block ml-1" style={{ color: primaryColor }}>
         Categorías
       </h2>
 
-      <Scrollspy className={styles.tabs} items={sectionIds} currentClassName={styles.itemActive}>
+      <Scrollspy
+        className={styles.tabs}
+        onUpdate={handleOnUpdate}
+        items={sectionIds}
+        currentClassName={styles.itemActive}
+      >
         {categories.map((category) => (
-          <li className={styles.item} key={category.id}>
+          <li className={styles.item} key={category.id} id={`li-${category.nombreCategoria}`}>
             <a className={styles.tabItem} href={`#${category.nombreCategoria}`}>
               {capitalize(category.nombreCategoria)}
             </a>
--- components/ProductList.js
+++ components/ProductList.js
@@ -3,8 +3,10 @@ import styles from 'styles/ProductList.module.sass';
 import ProductItem from 'components/ProductItem';
 
 const ProductList = ({ productCategory, productListData, onSelectProduct }) => (
-  <div className={`${styles.container}`} id={productCategory}>
-    <h2 className={styles.title}>{productCategory}</h2>
+  <div className={`${styles.container}`}>
+    <h2 className={styles.title} id={productCategory}>
+      {productCategory}
+    </h2>
     <div className="row">
       {productListData.map((product) => (
         <ProductItem key={product.id} product={product} onSelectProduct={onSelectProduct} />
