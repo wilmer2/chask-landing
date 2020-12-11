d255841e659047a5aa0ab29891f6c6b7451e47c9
Wilmer <wilmer.payall@gmail.com>
2020-12-11 11:38:40 -0400

    remove multiples backgrounds

--- components/ProductlModal.js
+++ components/ProductlModal.js
@@ -48,7 +48,7 @@ const ProductModal = ({ product, onCloseModal }) => {
             <figure
               className={styles.imageContainer}
               style={{
-                backgroundImage: `url(${product.urlImagen}), url(/assets/images/chaski_splash.png)`,
+                backgroundImage: `url(${product.urlImagen})`,
               }}
             />
 
